b21f2f0b334b5af00e124b11ec8b183c4a20c7cf
sujeongHeo401 <gotnwjd40@gmail.com>
2022-04-24 18:24:57 +0900

    Add mine-sweeper logic - 주변에 지뢰 없는 친구는 자동으로 뜨게

---
+++ main.py
@@ -0,0 +1,73 @@
+from tkinter import *
+from cell import Cell
+import settings
+import utils
+
+root = Tk()
+#Override the settings of the window
+root.configure(bg="black")
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+root.title("MineSweeper Game")
+root.resizable(False, False)
+
+top_frame=Frame(
+    root, 
+    bg='black', # Change later to black
+    width=settings.WIDTH,
+    height=180
+)
+
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root, 
+    bg='black', # Change later to black
+    width=utils.width_prct(25),
+    height=utils.height_prct(75)
+)
+
+left_frame.place(x=0, y=utils.height_prct(25))
+
+center_frame = Frame(
+    root,
+    bg='black', #Change later to black
+    width=utils.width_prct(75),
+    height=utils.height_prct(75)
+)
+
+center_frame.place(
+    x=utils.width_prct(25),
+    y=utils.height_prct(25),
+)
+
+for x in range(settings.GRID_SIZE): # 0, 1, 2, 3, 4
+    for y in range(settings.GRID_SIZE):
+        c = Cell(x, y)
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column = x, row = y
+        )
+Cell.randomize_mines()
+
+for c in Cell.all:
+    print(c.is_mine)
+# btn1 = Button(
+#     center_frame,
+#     bg='blue',
+#     text='First Button'
+# )
+# btn1.place(x=0, y=0)
+
+# c1 = Cell(x, y)
+# c1.create_btn_object(center_frame)
+# c1.cell_btn_object.grid(
+#     column = 0, row = 0
+# )
+
+# c2 = Cell()
+# c2.create_btn_object(center_frame)
+# c2.cell_btn_object.grid(
+#     column= 1, row = 0
+# )
+#Run the window
+root.mainloop()
